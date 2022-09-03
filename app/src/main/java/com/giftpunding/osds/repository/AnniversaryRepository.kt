e0362b06eb73fa1119279770e702e780d272809f
RyuSw-cs <rsw1452@naver.com>
2022-09-03 16:11:01 +0900

    feat: #67 기념일 서버 연결

--- app/src/main/java/com/giftpunding/osds/repository/AnniversaryRepository.kt
+++ app/src/main/java/com/giftpunding/osds/repository/AnniversaryRepository.kt
@@ -1,15 +1,16 @@
 package com.giftpunding.osds.repository
 
+import com.giftpunding.osds.data.response.anniversary.AnniversaryRequest
+import com.giftpunding.osds.data.response.anniversary.AnniversaryResponse
 import com.giftpunding.osds.data.response.user.User
 import com.giftpunding.osds.repository.remote.datasource.AnniversaryDataSource
+import retrofit2.Call
+import retrofit2.Response
 
 class AnniversaryRepository(private val anniversaryDataSource: AnniversaryDataSource) {
 
-    suspend fun addAnniversary(anniversaryDay: String, anniversary: String) {
-        val userDto = User(
-            anniversaryDay = anniversaryDay,
-            anniversary = anniversary
-        )
-        anniversaryDataSource.getAnniversaryService().postUser(userDto)
+    suspend fun addAnniversary(anniversaryDay: String, anniversary: String) : Response<AnniversaryResponse> {
+        val anniversaryRequest = AnniversaryRequest(date = anniversaryDay, type = anniversary)
+        return anniversaryDataSource.getAnniversaryService().postAnniversary(anniversaryRequest)
     }
 }
--- app/src/main/java/com/giftpunding/osds/repository/remote/service/AnniversaryService.kt
+++ app/src/main/java/com/giftpunding/osds/repository/remote/service/AnniversaryService.kt
@@ -1,10 +1,19 @@
 package com.giftpunding.osds.repository.remote.service
 
+import com.giftpunding.osds.data.response.anniversary.AnniversaryRequest
+import com.giftpunding.osds.data.response.anniversary.AnniversaryResponse
+import com.giftpunding.osds.data.response.login.LoginResponse
 import com.giftpunding.osds.data.response.user.User
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface AnniversaryService {
-    //@PUT
-    suspend fun postUser(user:User){
 
-    }
+    @POST("/api/anniversaries")
+    suspend fun postAnniversary(
+        @Body anniversaryRequest: AnniversaryRequest
+    ): Response<AnniversaryResponse>
 }
